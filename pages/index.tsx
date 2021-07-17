2ed9d114c93b594e4364ec34cbd08b84e2a93e88
NWylynko <nick@wylynko.com>
2021-07-17 11:55:05 +0000

    log events

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,28 @@
 import { useState } from "react"
 import Axios from "axios";
+import { v4 as uuid } from 'uuid';
 
 const { post } = Axios.create({
   baseURL: "/api/"
 })
 
+interface LogItem {
+  id: string;
+  timestamp: Date;
+  action: string;
+  message: string;
+}
+
 function HomePage() {
 
   const [message, setMessage] = useState("")
 
+  const [localEvents, setLocalEvents] = useState<LogItem[]>([])
+
   const sendMessage = async () => {
+    setLocalEvents(s => [...s, { id: uuid(), timestamp: new Date(), action: 'sendMessage', message }])
     const { data } = await post("/sendMessage", { message })
+    setLocalEvents(s => [...s, { id: uuid(), timestamp: new Date(), action: 'messageAdded', message }])
     console.log({ data })
   }
 
@@ -20,7 +32,24 @@ function HomePage() {
       <label>Send Message</label>
       <input value={message} onChange={e => setMessage(e.target.value)} />
       <button onClick={sendMessage} >Submit</button>
+      <h3>local Events</h3>
+      <Log log={localEvents} />
     </>
+  )
+}
+
+interface LogProps {
+  log: LogItem[];
+}
+
+const Log = ({ log }: LogProps) => {
+  const list = log.map((logItem) => (
+    <span key={logItem.id} >{logItem.timestamp.toTimeString().slice(0,8)} {logItem.action} {logItem.message}</span>
+  ))
+  return (
+    <div>
+      {list}
+    </div>
   )
 }
 
